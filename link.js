08642cbc52420ac8bca4bec1365c94191f8a5e6e
Henry Ngoy <henryngoy@yahoo.com>
2013-06-13 13:46:12 -0700

    Updated Video

--- link.js
+++ link.js
@@ -15,4 +15,16 @@ function onDeviceReady() {
 		window.open(encodeURI('http://www.mksecrets.net/mk2/eng/mk2-bios.php'), '_blank', 'location=yes');
 	});
 	
+	$('#mk2vid1').click(function() {
+		window.open(encodeURI('Images/Video /Mortal_Kombat_II_Fatalities.mp4'), '_blank', 'location=yes');
+	});
+	
 }
+
+function onDeviceReady() {
+	$('#kicharactermoves').click(function() {
+		window.open(encodeURI('http://killerinstinct.gamebub.com/characters.php'), '_blank', 'location=yes');
+	});
+	
+}
+
